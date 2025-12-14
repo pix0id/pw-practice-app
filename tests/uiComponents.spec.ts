4c332edc886a5e72744fc077603dc90855a8aeb5
Alex Sollman <alex@alexsollman.dev>
2025-12-13 16:24:46 -0800

    Starting Tables

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -154,6 +154,20 @@ test('dialog', async ({page}) => {
   await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
 })
 
-test('tables', async ({page}) => {
+test('web tables', async ({page}) => {
+    await page.getByText("Tables & Data").click();
+    await page.getByText("Smart Table").click();
 
+    // 1. how to get the row by any text in the row
+    /*const targetRow = page.getByRole("row", {name: "twitter@outlook.com"})
+    await targetRow.locator(".nb-edit").click();
+
+    await page.locator('input-editor').getByPlaceholder('Age').clear()
+    await page.locator('input-editor').getByPlaceholder('Age').fill('36')
+    await page.locator('.nb-checkmark').click();*/
+
+    // 2 get the row based on the value in a specific column
+    await page.locator('.ng2-smart-pagination-nav').getByText('2').click();
+    const targetRowById = page.getByRole('row', {name: '11'}).filter({has: page.locator('td').nth(1).getByText('11')})
+    await targetRowById.locator('.nb-edit').click();
 })
