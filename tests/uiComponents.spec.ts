b0d5b724f7dc99cd8b30927a6f9c40cd29d2f2d5
Alex Sollman <alex@alexsollman.dev>
2025-12-13 16:45:45 -0800

    Finishing web tables part 1

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -170,4 +170,9 @@ test('web tables', async ({page}) => {
     await page.locator('.ng2-smart-pagination-nav').getByText('2').click();
     const targetRowById = page.getByRole('row', {name: '11'}).filter({has: page.locator('td').nth(1).getByText('11')})
     await targetRowById.locator('.nb-edit').click();
+
+    await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+    await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
+    await page.locator('.nb-checkmark').click();
+    await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
 })
