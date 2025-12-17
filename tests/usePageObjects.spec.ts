1a0b48e626a27bd2bbba3f93f71bd91c3591d880
Alex Sollman <alex@alexsollman.dev>
2025-12-17 10:16:52 -0800

    Added more methods for navigation, as well as a smart method to deterimine if a group is collapsed or not.

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,11 +1,16 @@
-import {test, expect} from '@playwright/test'
-import {NavigationPage} from "../page-objects/navigationPage";
+import { test, expect } from "@playwright/test";
+import { NavigationPage } from "../page-objects/navigationPage";
 
 test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:4200");
 });
 
-test('navigate to form page', async ({ page }) => {
-    const navigateTo = new NavigationPage(page)
-    await navigateTo.formLayoutsPage()
-})
+test("navigate to form page", async ({ page }) => {
+    const navigateTo = new NavigationPage(page);
+
+    await navigateTo.formLayoutsPage();
+    await navigateTo.datePickerPage();
+    await navigateTo.smartTablePage();
+    await navigateTo.toastrPage();
+    await navigateTo.tooltipPage();
+});
