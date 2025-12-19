576e8f14f21deb4faa534a417d3842f3b7c43afe
Alex Sollman <alex@alexsollman.dev>
2025-12-18 23:41:27 -0800

    New form layouts page object model, adding tests for this page.

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { NavigationPage } from "../page-objects/navigationPage";
+import { FormLayoutPage } from "../page-objects/formLayoutsPage";
 
 test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:4200");
@@ -14,3 +15,12 @@ test("navigate to form page", async ({ page }) => {
     await navigateTo.toastrPage();
     await navigateTo.tooltipPage();
 });
+
+test("Parameterized methods", async ({ page }) => {
+    const navigateTo = new NavigationPage(page);
+    const onFormsLayoutsPage = new FormLayoutPage(page);
+
+    await navigateTo.formLayoutsPage();
+    await onFormsLayoutsPage.submitGridFormCredsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
+    await onFormsLayoutsPage.submitInlineForm('Alex Sollman', 'test2@test2.com', true)
+})
