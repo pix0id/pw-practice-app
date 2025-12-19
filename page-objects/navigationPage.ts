9082318dc67a907208eb45d9744b7ace1a79b91e
Alex Sollman <alex@alexsollman.dev>
2025-12-18 18:11:49 -0800

    Separating locators from methods

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,35 +1,46 @@
-import { Page } from "@playwright/test";
+import {Locator, Page} from "@playwright/test";
 
 export class NavigationPage {
     readonly page: Page;
+    readonly formLayoutMenuItem: Locator
+    readonly datePickerMenuItem: Locator
+    readonly smartTableMenuItem: Locator
+    readonly toastrMenuItem: Locator
+    readonly tooltipMenuItem: Locator
 
     constructor(page: Page) {
         this.page = page;
+        this.formLayoutMenuItem = page.getByText("Form Layouts")
+        this.datePickerMenuItem = page.getByText("Datepicker")
+        this.smartTableMenuItem = page.getByText("Smart Table")
+        this.toastrMenuItem = page.getByText("Toastr")
+        this.tooltipMenuItem = page.getByText("Tooltip")
     }
+
     async formLayoutsPage() {
         await this.selectGroupMenuItem("Forms");
-        await this.page.getByText("Form Layouts").click();
+        await this.formLayoutMenuItem.click();
     }
 
     async datePickerPage() {
         await this.selectGroupMenuItem("Forms");
-        this.page.waitForTimeout(1000);
-        await this.page.getByText("Datepicker").click();
+        await this.page.waitForTimeout(1000);
+        await this.datePickerMenuItem.click();
     }
 
     async smartTablePage() {
         await this.selectGroupMenuItem("Tables & Data");
-        await this.page.getByText("Smart Table").click();
+        await this.smartTableMenuItem.click();
     }
 
     async toastrPage() {
         await this.selectGroupMenuItem("Modal & Overlays");
-        await this.page.getByText("Toastr").click();
+        await this.toastrMenuItem.click();
     }
 
     async tooltipPage() {
         await this.selectGroupMenuItem("Modal & Overlays");
-        await this.page.getByText("Tooltip").click();
+        await this.tooltipMenuItem.click();
     }
 
     // smart method that will check if the group is expanded or not
